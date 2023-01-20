515abdcc16f0b27fd5d90894ae9d46dc446b1550
Shouko <45937104+Ringooh@users.noreply.github.com>
2023-01-20 13:27:12 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// load env file (contains important keys)
+require('dotenv').config();
+const fs = require('fs');
+
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
+const eventHandler = require('./handlers/eventHandler');
+const commandHandler = require('./handlers/handler');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ],
+
+    prefix  : 'f!'
+});
+
+client.on('ready', () => {
+    client.user.setPresence({
+        activities: [{
+            type: ActivityType.Competing,
+            name: 'in rank up',
+            status: "online"
+        }]        
+    });
+});
+
+eventHandler(client);
+commandHandler(client);
+
+client.login(process.env.DISCORD_TOKEN);
+
